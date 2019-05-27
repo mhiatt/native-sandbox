669712b76cd3903ba00a4f9097506affb5b10262
Myles Hiatt <myles.hiatt@gmail.com>
2019-05-26 20:36:35 -0400

    Init commit

---
+++ screens/MapScreen.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import { MapView } from 'expo';
+
+const { Marker } = MapView;
+
+import publicEvents from '../mockData/publicEvents.json';
+
+class MapScreen extends Component {
+  render() {
+    console.log(Marker);
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+          showsUserLocation: true
+        }}
+      >
+        {
+          publicEvents.map((event) => {
+            return (
+              <Marker
+                key={event.id}
+                coordinate={{
+                  latitude: event.lat,
+                  longitude: event.long
+                }}
+                title={event.eventName}
+                description={event.eventDescription}
+              />
+            );
+          })
+        }
+
+      </MapView>
+    );
+  }
+}
+
+export default MapScreen;
